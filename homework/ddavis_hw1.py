9a297184f1caee094d2745507b10054636eecd95
phantomamoeba <polonius@davistx.com>
2018-02-09 17:30:43 -0600

    hw1

--- homework/ddavis_hw1.py
+++ homework/ddavis_hw1.py
@@ -71,12 +71,12 @@ def Psi(r,a,gamma):
     return -1*Phi(r,a,gamma)/(G*M/a)
 
 #def bE(E,a=1): #dimensionless binding energy
-    #return -E/(2.*G*M/(3.*a))
+    #return -E/(2.*G*M/(3.*a)) #??typo (1.* a not 3*a?)
 
 def E_c(r,a,gamma): #E_c but is generally true for any orbit? Not just circular (or,at least for this purpose)
 
     y_c = r/(r+a)
-    return 1./(2.-gamma) - (4.-gamma)/(2.*(2.-gamma))*y_c**(2.-gamma) + 0.5*y_c**(3.-gamma)
+    return (1./(2.-gamma) - (4.-gamma)/(2.*(2.-gamma))*y_c**(2.-gamma) + 0.5*y_c**(3.-gamma)) #*(0.5)
 
 
 def y_func(r,a,gamma): #really feel dumb naming this way ...
@@ -106,7 +106,9 @@ def g_integrand(psi,E,r,gamma,a):
 
 def df_g(r,a,gamma): #distribution function of g(E)
     psi = Psi(r,a,gamma)
-    psi_0 = Psi(0,a,gamma)
+    psi_0 = Psi(0,a,gamma) #for gamma = 3/2 is constant 2.0
+  #  psi_0 = 1.0 #from the Dehen figure 5
+   # psi /= 2.
     E = E_c(r,a,gamma)
     return 16.*np.pi**2.*np.sqrt(2.*G*M*a**5)*quad(g_integrand,E,psi_0,args=(E,r,gamma,a))[0]
 
@@ -130,25 +132,36 @@ def plot_f_g(r_i,r_f,gridsize,a,gamma):
     psd = np.array(psd)
     dos = np.array(dos)
 
-    plt.title("Phase Space Density and Density of States (Prob#2,3,4)\n$\gamma$=3/2")
 
-    plt.ylabel("$log_{10}(func)$")
-    #plt.gca().set_yscale("log")
+    fig = plt.figure(figsize=(6,9))
+    fig.subplots_adjust(hspace=.5)
+    fig.subplots_adjust(wspace=.5)
+
+    plt.subplot(211)
+    plt.title("Phase Space Density and Density of States (Prob#2,3),  $\gamma$=3/2")
+    plt.ylabel(r"f($\mathscr{E}$), g($\mathscr{E}$)")
+    plt.gca().set_yscale("log")
     #since script E = -E*a/(2GM), mult by -0.5 to scale
-    if True:
-        plt.xlabel("$\mathscr{E}$")
-        plt.plot(-0.5 * E_c(grid,a,gamma),np.log10(psd),label=r'f($\mathscr{E}$)')
-        plt.plot(-0.5 * E_c(grid, a, gamma), np.log10(dos),label=r'g($\mathscr{E}$)')
-        plt.plot(-0.5 * E_c(grid,a,gamma),np.log10(psd*dos),label=r'f($\mathscr{E}$) $\cdot$ g($\mathscr{E}$)')
-    else:
-        plt.xlabel("r")
-        #plt.plot(grid, psd, label=r'f($\mathscr{E}$)')
-        #plt.plot(grid, dos, label=r'g($\mathscr{E}$)')
-        plt.plot(grid, psd * dos, label=r'f($\mathscr{E}$) $\cdot$ g($\mathscr{E}$)')
 
+    plt.xlabel(r"$\mathscr{E}$  ($\leftarrow$ $r^{+}$)")
+    plt.plot( 0.5* E_c(grid,a,gamma),psd,label=r'f($\mathscr{E}$)')
+    plt.plot( 0.5* E_c(grid, a, gamma), dos,label=r'g($\mathscr{E}$)')
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, 0.98), borderaxespad=0)
 
-    #plt.savefig("dd_hw1p234.png")
+
+    plt.subplot(212)
+    plt.title(r"$\frac{dM}{d\mathscr{E}}$ vs $\mathscr{E}$ (Prob#4), $\gamma$=3/2")
+    plt.ylabel(r"$dM/d\mathscr{E}$")
+    plt.gca().set_yscale("log")
+    plt.xlabel(r"$\mathscr{E}= \frac{-aE}{2GM}$    ($\leftarrow$ $r^{+})$")
+    plt.plot( 0.5 * E_c(grid, a, gamma), (psd * dos))#, label=r'f($\mathscr{E}$) $\cdot$ g($\mathscr{E}$)')
+
+    #plt.xlabel("r")
+    #plt.plot(grid, psd * dos)
+
+
+
+    plt.savefig("dd_hw1p234.png")
     plt.show()
 
 
@@ -158,7 +171,7 @@ def main():
     #will get a warning about convergence at the 0 limit, but is okay
     #plot_sigma(10**(-5),100,1000,1,1.5)
 
-    plot_f_g(10**-5, 100000, gridsize=1000, a=1, gamma=1.5)
+    plot_f_g(10**-10, 10**4, gridsize=1000, a=1, gamma=1.5)
 
 if __name__ == '__main__':
     main()
