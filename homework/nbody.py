63d8aa873e15c996c729083eb2633a1b6d573774
phantomamoeba <polonius@davistx.com>
2018-05-04 09:53:02 -0500

    final?

--- homework/nbody.py
+++ homework/nbody.py
@@ -10,6 +10,7 @@ from scipy.integrate import quad
 from scipy.integrate import nquad
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import sys
 
 pc2cm = 3.08567758e18
 M_sun2grams = 1.989e33
@@ -17,11 +18,10 @@ a_kpc=35
 a_cm=a_kpc*1e3*pc2cm #scale length aka r_s
 M_tot_M_sun = 1e12
 M_tot_grams = M_tot_M_sun * M_sun2grams
-TotalNumParticles = int(1e3) #all particles of the same mass (set low for testing)
+TotalNumParticles = int(1e6) #all particles of the same mass (set low for testing)
 ParticleMass = M_tot_grams / TotalNumParticles
 G = 6.67428*10**(-8) #cgs
 rho_0 = 1.0 #should be something like rho_200? or rhocrit?
-
 
 v_g = np.sqrt(G*M_tot_grams/a_cm)
 Max_E = -G*M_tot_grams/a_cm
@@ -70,8 +70,6 @@ def mass_enclosed(radius): #basically my cdf
     #since can analytically integrate, just use that instead
     return M_tot_grams*(radius/(radius + a_cm))**2
 
-#todo: the translation is okay, but uniform random is wrong ...
-#todo: you end up with equal numbers of particles at all radii (because of the uniform random)
 def random_radius_cm():
     #make the upper limit a bit less than 1 since that would correspond to r=infinity
     #max_x = mass_enclosed(100*a_cm)/M_tot_grams
@@ -107,12 +105,13 @@ class Particle():
         self.mass_enclosed = mass_enclosed(self.r)
 
     def __str__(self):
-        return "%f %f %f %f %f %f" %(self.x, self.y, self.z, self.v_x, self.v_y, self.v_z)
+        return "%d %d %d %f %f %f" %(self.x, self.y, self.z, self.v_x, self.v_y, self.v_z)
 
     def spherical(self):
         return "%f %f %f" %(self.r/pc2cm/1e3, 90.0 - self.theta*180.0/np.pi, self.phi*180.0/np.pi)
 
 
+#generic ... ended up not using this version ... see next function below
 def pdf_sample_accept_reject(pdf, samples=1, x_min=0, x_max=1, pdf_probe=int(1e3), pmf_x=None, pmf_y=None):
 
     #if the pdf is a pmf, supply the x and y arrays
@@ -157,56 +156,59 @@ def pdf_sample_accept_reject(pdf, samples=1, x_min=0, x_max=1, pdf_probe=int(1e3
     return rvs_x, rvs_y
 
 
+#specific for the way I am calculating f(E)
+def f_E_sample_accept_reject(pdf, radius, samples=1):
+
+
+    f_E, E = pdf(radius)
+    pdf_min = 0.
+    pdf_max = np.max(f_E) #pdf(radius)[0].max() #[0] is f(e), [1] is E
+
+    rvs_x = np.zeros(samples)
+    rvs_y = np.zeros(samples)
+    accepts = 0
+    rejects = 0 #not really using this right now, but for future might want ratio with accepts?
+
+    while accepts < samples:
+        x_bid = np.random.uniform(Hernquist_potential(radius), 0) #E not r
+        y_bid = np.random.uniform(pdf_min, pdf_max)
+
+
+        i = getnearpos(E,x_bid)
+
+        #if y is under the pdf curve ...
+        if y_bid < f_E[i]:
+            rvs_x[accepts] = x_bid
+            rvs_y[accepts] = y_bid
+            accepts += 1
+            #print("Samples = %d" %accepts)
+        else:
+            rejects += 1
+
+    return rvs_x, rvs_y
+
+
 
 #def q(E):
 #    return np.sqrt(-1/v_g**2 * E)
 
-def q(radius,v):
-    return np.sqrt(-1/v_g**2 * E_tot(radius,v))
-
-#from MBK
-def E_tot(radius,v): #per unit mass NOT BINDING E
-    return phi_potential(radius) + 0.5*v**2
-
-
-
-def check_q(radius,v):
-    E =  E_tot(radius,v)
-    if E > 0:
-        return False
-
-    q = np.sqrt(-1 / v_g ** 2 * E)
-    if (q < 0) or (q > 1):
-        return False
-    else:
-        return True
-
-# def f(radius,v): #f(E) as f(v)
-#     #still need to calc v .... we want v back
-#     #can we solve for v?
-#     q = np.sqrt(-1/v_g**2 * E_tot(radius,v))
+# def q(radius,v):
+#     return np.sqrt(-1/v_g**2 * E_tot(radius,v))
 #
-#     #if (q < 0) or (q > 1):
-#     #    return None
+# def E_tot(radius,v):
+#     return phi_potential(radius) + 0.5*v**2
+
 #
-#     return M_tot_grams/(8*np.sqrt(2)*(np.pi*a_cm*v_g)**3)*1/(1-q**2)**(2.5) * \
-#     (3 * np.arcsin(q) + q*np.sqrt((1-q**2))*(1-2*q**2)*(8*q**4-8*q**2-3))
-
-
-#let E vary between max(negative) and 0
-#probe (accept-reject) to get random E for this radius, then solve for |v|
-def f_E(): #f(E) as f(v)
-    E_grid = np.linspace(Max_E,0,1000)
-    q = np.sqrt(-1/v_g**2 * E_grid)
-
-    return M_tot_grams/(8*np.sqrt(2)*(np.pi*a_cm*v_g)**3)*1/(1-q**2)**(2.5) * \
-    (3 * np.arcsin(q) + q*np.sqrt((1-q**2))*(1-2*q**2)*(8*q**4-8*q**2-3))
-
-
-
-
-
-# E = phi - 1/2 v**2  --> v = sqrt(2*(phi-E))
+# def check_q(radius,v):
+#     E =  E_tot(radius,v)
+#     if E > 0:
+#         return False
+#
+#     q = np.sqrt(-1 / v_g ** 2 * E)
+#     if (q < 0) or (q > 1):
+#         return False
+#     else:
+#         return True
 
 #f(E) for a given radius .... E cannot be > phi_potential else would be unbounded
 #right ... so need the WHOLE potential (checking vs unbound) not just enclosed
@@ -221,44 +223,18 @@ def f(radius):
 
     return f_E, E_grid
 
-#should be for a given radius and energy
-def f_at_E(v,radius): #f(E) as f(v,r)
-    q = np.sqrt(-1/v_g**2 * E_tot(radius,v))
-
-    return M_tot_grams/(8*np.sqrt(2)*(np.pi*a_cm*v_g)**3)*1/(1-q**2)**(2.5) * \
-    (3 * np.arcsin(q) + q*np.sqrt((1-q**2))*(1-2*q**2)*(8*q**4-8*q**2-3))
-
-#integrated at a given r over all (valid) velocities
-#so can check, for a given radius what are the possible Energies (and then to the corresponding velocity)
-def int_f_at_E(radius):
-    #only want to look at negative E_tots, so find max velocity that still results in negative E_tot
-    v_grid = np.linspace(0,1e8,10000)
-    Es = E_tot(radius,v_grid)
-    i = np.argmax([e for e in Es if e < 0])
-
-    return quad(f_at_E, 0, v_grid[i],args=(radius))[0], v_grid[0:i+1]
-
 
 def velocity(E,radius):
-    if (phi_potential(radius)-E) < 0:
+    if (E-Hernquist_potential(radius)) < 0: #should never happen
         print ("Error in velocity: E = %f, R = %f" % (E,radius))
-    return np.sqrt(2*(phi_potential(radius)-E))
+    return np.sqrt(2*(E-Hernquist_potential(radius)))
 
 #based only on valid q ... not sampling from radius/energy pmf
-def random_velocity(radius):
-    v = np.random.uniform(0,5e7) #0 to 500 km/s in cm/2
-    while not check_q(radius, v):
-        v = np.random.uniform(0,5e7) #0 to 500 km/s in cm/2
-    return v
-
-
-def radius_energy_dist():
-    r_grid = np.linspace(1, 1e6 * pc2cm, 10000)
-    fe_grid = []
-    for r in r_grid:
-        fe_grid.append(int_f_at_E(r)[0])
-
-    return r_grid, fe_grid
+# def random_velocity(radius):
+#     v = np.random.uniform(0,5e7) #0 to 500 km/s in cm/2
+#     while not check_q(radius, v):
+#         v = np.random.uniform(0,5e7) #0 to 500 km/s in cm/2
+#     return v
 
 
 def getnearpos(array,value):
@@ -270,85 +246,27 @@ def re_pmf(r_grid,e_grid,r):
     return e_grid[i]
 
 def main():
-
-
     #for each particle
     #get its radius, then angular position
     #then translate to cartesian
-    #get its velocity
+    #get its velocity (random sample from f(E)
     #write out to file (x,y,z,v_x,v_y,v_z)
 
-    f_E, E = f(a_cm)
-    plt.plot(E, f_E)
-    plt.show()
-    exit()
-
-
-
-    #test
-    print("testing int_f_e")
-    radius = 100*a_cm
-    v_grid = np.linspace(0, 1e8, 10000)
-    Es = E_tot(radius, v_grid)
-    i = np.argmax([e for e in Es if e < 0]) + 1 #+1 so don't have the +1 in the slice later
-
-    E_grid=E_tot(radius,v_grid[0:i])
-
-    plt.plot(v_grid[0:i]/1e5,np.log10(f_at_E(v_grid[0:i],radius)))
-    plt.plot()
-    plt.show()
-    exit()
-
-
-    print("Building radius, energy pmf ()...")
-    #for each r, get the max energy (s|t sqrt arg is > 0)
-    r_grid, e_grid = radius_energy_dist()
-    plt.plot(r_grid/a_cm, e_grid)
-    plt.show()
-
+    #sanity check f(E) for a radius
+    # f_E, E = f(a_cm)
+    # plt.plot(E, f_E)
+    # plt.show()
+    # exit()
 
     #get all particle coords
-    print("Building particle locations (~15-20seconds) ...")
+    print("Building particles and writing to particles.dat ...")
     particles = []
+    outfile = open("particles.dat","w")
+    outfile.write("# x y z vx vy vz  in cm and cm/s\n")
     for i in range(int(TotalNumParticles)):
-        p = Particle() #auto-pops a random position
-        particles.append(p)
-
-       # particles.append(Particle())
-        #print(particles[i].spherical())
-        #print(particles[i])
-
-    #done this way just so we can chop up the progress
-    print("Building particle velocities ...")
-    for p in particles:
-        #todo: .....
-        #no, what we want if for each particle (radius) what are the possible bound Energies?
-        #and make a pdf or pmf of those energies
-        #then select from that distribution
-        #
-        # so ... each radius gets its own pdf?
-        # or ... uniform select an Energy at a radius between 0 and the max(negative) energy?
-
-
-
-        # get a random energy for this particle from the distribution
-        #print("Sampling pmf at %g a" %(p.r/a_cm))
-        #_,fe = pdf_sample_accept_reject(re_pmf, x_min=p.r, x_max=p.r, samples=1, pmf_x=r_grid, pmf_y=e_grid)
-
-        #got back f(E) ... need to get to E
-        #print (fe[0],p.r)
-
-
-        #todo: translate f(E) to back to E
-        #need f(E) v E
-        e = 0
-
-
-        #this part is okay
-        # get the corresponding velocity from the radius and energy
-        v = velocity(e, p.r)
-
-        # v = random_velocity(p.r)
+        p = Particle() #auto-pops a random position uniform on sphere with random radius
+        sample_E, sample_fE = f_E_sample_accept_reject(f,radius=p.r,samples=1)
+        v = velocity(sample_E, p.r)
         # so v is okay now
         # get a random direction for v
         theta, phi = random_on_sphere()
@@ -358,16 +276,57 @@ def main():
         p.v_y = y * v
         p.v_z = z * v
 
+        outfile.write(str(p)+"\n")
+        particles.append(p)
 
+        percent = (i+1)/TotalNumParticles*100.
+        sys.stdout.write('\r')
+        sys.stdout.write("[%-20s] %g%% (N=%d)" % ('=' * int(percent/5.0), percent,i+1))
+        sys.stdout.flush()
+
+    print("\n")
+    outfile.close()
+    print("Particles built.")
+
+    #check velocity dispersion
+    #bin by radii
+    #calc std.dev of velocity in bin (on v_x or v_y or_v_z) and plot
+
+    print("Binning dispersion ...")
+    #not pythonic way to do this but I'm tired and not thinking straight
+    num_bins = 100
+    max_r = np.linspace(1,100,num_bins) #in kpc
+    max_r = np.append(max_r,[100000])
+    bins_vx = [[] for i in range(num_bins+1)]
+    for p in particles:
+       # i = np.argmax([e for e in Es if e < 0])
+        i = int(p.r / (pc2cm*1e3))
+        if i > num_bins:
+            i = num_bins
+        bins_vx[i].append(p.v_x)
+
+    sd = np.zeros(num_bins+1)
+    for i in range(num_bins+1):
+        sd[i] = np.std(bins_vx[i])/1e5 #cm/s to km/s
+
+    plt.title("Velocity Dispersion")
+    plt.ylabel("Dispersion [km/s]")
+    plt.xlabel("Radius in kpc")
+    plt.plot(max_r[:-1],sd[:-1])
+    plt.savefig("dispersion.png")
+    plt.show()
 
     #sanity check velocity distribution is spherical
-    print("building velocity distro plot")
+    print("building velocity distro plot ")
     fig = plt.figure()
+
     ax = fig.add_subplot(111, projection='3d')
+    fig.suptitle("First 1000 (random) velocities (normed by v_g) ")
     #since particles is random already, just plot the first 1000
-    x = [p.v_x/v_g for p in particles[0:1000]]
-    y = [p.v_y/v_g for p in particles[0:1000]]
-    z = [p.v_z/v_g for p in particles[0:1000]]
+    maxidx = min(1000,len(particles))
+    x = [p.v_x/v_g for p in particles[0:maxidx]]
+    y = [p.v_y/v_g for p in particles[0:maxidx]]
+    z = [p.v_z/v_g for p in particles[0:maxidx]]
 
     ax.scatter(x,y,z)
     plt.show()
@@ -389,8 +348,6 @@ def main():
     # plt.title("Accept-Reject method") #check this vs inverse method
     # plt.hist(np.array(rvs)/a_cm, bins=1000, cumulative=True, density=True, range=[0, 100])
     # plt.show()
-
-
 
 
 if __name__ == '__main__':
